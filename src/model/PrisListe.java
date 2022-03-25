a19aafaff5ce8153aed2b39dd572e0936d0fa119
Malte <eaamaoa@students.eaaa.dk>
2022-03-25 09:14:41 +0100

    Commit

---
+++ src/model/PrisListe.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PrisListe {
+    private String navn;
+
+    public String PrisListe(String navn) {
+        ProduktPris produktPris;
+        Produkt produkt;
+
+
+    }
+}
