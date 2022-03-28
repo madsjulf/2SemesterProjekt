93442cbf57f2e0bddf715ecf7d8c6bc89caf1f60
Malte <eaamaoa@students.eaaa.dk>
2022-03-28 11:46:37 +0200

    Commit

---
+++ src/GUI/ProduktWindow.java
@@ -0,0 +1,105 @@
+package GUI;
+
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import model.PrisListe;
+import model.ProduktGruppe;
+
+public class ProduktWindow extends Stage {
+    private final TextField txfName = new TextField();
+    private final ComboBox comboBoxProduktGruppe = new ComboBox();
+    private final ComboBox comboBoxPrisListe = new ComboBox();
+    private final Label lblError = new Label();
+
+public ProduktWindow(String title, String name, ProduktGruppe produktGruppe) {
+
+
+    this.setTitle(title);
+    GridPane pane = new GridPane();
+    this.initContent(pane);
+
+    Scene scene = new Scene(pane);
+    this.setScene(scene);
+}
+
+//-----------------------------------------------------------------------------------
+
+    private void initContent(GridPane pane) {
+        pane.setPadding(new Insets(10));
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setGridLinesVisible(false);
+
+
+        // Label og textfeldt til navn på produkt
+        Label lblName = new Label("Name");
+        pane.add(lblName, 0, 0);
+        pane.add(txfName, 0, 1);
+        txfName.setPrefWidth(200);
+
+
+
+        // Label og combobox til liste af produktgrupper
+        Label lblProduktGruppe = new Label("ProduktGrupper");
+        pane.add(lblProduktGruppe, 1, 1);
+        pane.add(comboBoxProduktGruppe, 1, 2);
+
+
+        //Label og combobox til liste af Prislister
+        Label lblPrisListe = new Label("PrisLister");
+        pane.add(lblPrisListe, 2, 3);
+        pane.add(comboBoxPrisListe, 2, 4);
+
+
+        // error besked
+        pane.add(lblError, 0, 22);
+        lblError.setStyle("-fx-text-fill: red");
+
+        // cancel knap
+        Button btnCancel = new Button("Cancel");
+        pane.add(btnCancel, 0, 20);
+        GridPane.setHalignment(btnCancel, HPos.LEFT);
+        btnCancel.setOnAction(event -> this.cancelAction());
+
+        // ok knap
+        Button btnOK = new Button("OK");
+        pane.add(btnOK, 0, 21);
+        GridPane.setHalignment(btnOK, HPos.RIGHT);
+        btnOK.setOnAction(event -> this.okAction());
+    }
+
+//--------------------------------------------------------------------------------
+
+    private void cancelAction() {
+        this.hide();
+    }
+
+    private void okAction() {
+        String name = txfName.getText().trim();
+        if (name.length() == 0) {
+            lblError.setText("Navn er tomt!");
+            return;
+        }
+
+        ProduktGruppe produktGruppe = (ProduktGruppe) comboBoxProduktGruppe.getSelectionModel().getSelectedItem();
+
+        if (produktGruppe == null) {
+            lblError.setText("Giv produktet en produktgruppe.");
+            return;
+        }
+
+        PrisListe prisListe = (PrisListe) comboBoxPrisListe.getSelectionModel().getSelectedItem();
+
+
+
+        this.hide();
+    }
+
+}
