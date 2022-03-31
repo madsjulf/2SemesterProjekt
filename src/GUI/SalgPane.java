9386e523f544a878e06161f378b22d86fcbc513e
Malte <eaamaoa@students.eaaa.dk>
2022-03-31 09:04:45 +0200

    Commit

---
+++ src/GUI/SalgPane.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+
+public class SalgPane extends GridPane {
+
+
+
+
+    public SalgPane() {
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+
+
+    }
+
+
+
+    public void updateControls() {
+
+    }
+
+}
